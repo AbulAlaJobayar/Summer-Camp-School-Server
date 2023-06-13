f191505d7e26163c610aeceadab5c91c8339db76
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-06-13 11:51:39 +0600

    admin status update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.ENV_DB_USER}:${process.env.ENV_DB_PASS}@cluster0.ph1akes.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -76,6 +76,27 @@ async function run() {
     });
     app.get("/postdata/:email", async (req, res) => {
       const result = await instructorCollection.find({ instructoremail: req.params.email }).toArray();
+      res.send(result);
+    });
+    // admin api
+app.get("/alldata", async(req,res)=>{
+  const result=await instructorCollection.find().toArray();
+  res.send(result)
+})
+
+
+    app.put("/updatedata/:id",async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = req.body;
+      const update = {
+        $set: {
+          status: updateDoc.status,
+          
+        },
+      };
+      const result = await instructorCollection.updateOne(filter, update, options);
       res.send(result);
     });
 
