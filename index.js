2ad3109377a55d0e70a9f5817be64cb29b9ee30d
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-06-15 20:49:30 +0600

    all instructor api

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId, Collection } = require("mongodb");
 const uri = `mongodb+srv://${process.env.ENV_DB_USER}:${process.env.ENV_DB_PASS}@cluster0.ph1akes.mongodb.net/?retryWrites=true&w=majority`;
@@ -41,6 +62,14 @@ const studentsCollection = client.db("powerlearn").collection("student")
 
     const teacherCollection = client.db("powerlearn").collection("teacher");
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
+
+      res.send({ token })
+    })
+    
+
 
     //  save user
     app.put("/users/:email", async (req, res) => {
@@ -52,7 +81,7 @@ const studentsCollection = client.db("powerlearn").collection("student")
         $set: user,
       };
       const result = await usersCollection.updateOne(query, updateDoc, options);
-      console.log(result);
+      
       res.send(result);
     });
 
@@ -93,7 +122,11 @@ const studentsCollection = client.db("powerlearn").collection("student")
       const result = await instructorCollection.find({ instructoremail: req.params.email }).toArray();
       res.send(result);
     });
-
+    // get all instructor
+app.get('/instructor',async(req,res)=>{
+  const result = await usersCollection.find({role:"instructor"}).toArray();
+  res.send(result)
+})
 
 
     // admin api
@@ -172,6 +205,35 @@ app.get("/studentdatas",async(req, res) => {
       const result = await studentsCollection.find(query).toArray();
       res.send(result);
 })
+
+// student delete api
+app.delete("/coursedelete/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await studentsCollection.deleteOne(query);
+  res.send(result);
+});
+
+
+
+
+// payment api
+app.post('/create-payment-intent',verifyJWT, async (req, res) =>{
+  const {price} = req.body;
+  
+  const amount=parseInt(price*100);
+  console.log(amount);
+
+  const paymentIntent =await stripe.paymentIntents.create({
+    amount:amount,
+    currency:'usd',
+    payment_method_types:['card']
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+})
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
